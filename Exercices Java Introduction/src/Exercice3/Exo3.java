be3fd209f15a3c95e5b4cbf24c43ed210c3ff544
Nassim2 <45844813+Nassim2@users.noreply.github.com>
2018-12-13 11:54:21 +0100

    Update Exo3.java

--- Exercices Java Introduction/src/Exercice3/Exo3.java
+++ Exercices Java Introduction/src/Exercice3/Exo3.java
@@ -1,4 +1,24 @@
 package Exercice3;
 
+import java.util.ArrayList;
+
 public class Exo3 {
+
+    public static double average(String nbs) {
+        Integer sum = 0;
+        char[] nb_arr = nbs.toCharArray();
+        try {
+            for (int i = 0; i < nb_arr.length; i++) {
+                sum += Character.getNumericValue(nb_arr[i]);
+            }
+            return sum.doubleValue() / nb_arr.length;
+        } catch (NumberFormatException nfe) {
+            System.out.println("Values are incorrect");
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        average("1256783920495");
+    }
 }
