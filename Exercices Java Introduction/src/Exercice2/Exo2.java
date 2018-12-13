d680bb5953c4147fab29a81c826569af1780872b
Nassim2 <45844813+Nassim2@users.noreply.github.com>
2018-12-13 11:33:15 +0100

    Exercice 2 final commit

--- Exercices Java Introduction/src/Exercice2/Exo2.java
+++ Exercices Java Introduction/src/Exercice2/Exo2.java
@@ -1,4 +1,54 @@
 package Exercice2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Exo2 {
+    public static double average(String fileName) {
+        ArrayList<Integer> array = new ArrayList<>();
+        Integer sum = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] res = line.split(":");
+                array.add(Integer.parseInt(res[1]));
+            }
+            for (Integer mark : array) {
+                sum += mark;
+            }
+            return sum.doubleValue() / array.size();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static double getMarkEmail(String fileName, String Email) {
+        BufferedReader reader;
+        Integer res = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parse = line.split(":");
+                if (Objects.equals(parse[0], Email)) {
+                    res += Integer.parseInt(parse[1]);
+                }
+            }
+            return res;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        double average = average("C:\\Projects\\Exercice 2\\src\\Exo2\\test.txt");
+        double getMark = getMarkEmail("C:\\\\Projects\\\\Exercice 2\\\\src\\\\Exo2\\\\test.txt", "test.test@gmail.com");
+        System.out.println(average);
+        System.out.println(getMark);
+    }
 }
+
